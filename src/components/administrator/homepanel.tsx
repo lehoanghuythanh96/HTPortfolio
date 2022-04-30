d7744b12f4df3f25c20dbc9c635af39f19b1c167
Lê Hoàng Huy Thanh <93971600+lehoanghuythanh96@users.noreply.github.com>
2022-04-30 11:44:23 +0200

    turn homepanel to functional component

--- src/components/administrator/homepanel.tsx
+++ src/components/administrator/homepanel.tsx
@@ -4,39 +4,46 @@ import {BasicButton, SuccessButton} from "../UI_Components/Buttons";
 import {Link} from "react-router-dom";
 import {apiPostData} from "../../environments/apiHandler";
 import {MyBreadCrumb} from "../UI_Components/UI_Breadcrumbs";
+import {gql, useQuery} from "@apollo/client";
 
-export class AdminHomePage extends React.Component {
+const GET_POSTS = gql`
+  query GetDogs {
+    dogs {
+      id
+      breed
+    }
+  }
+`;
 
-    async deleteAlltrashMedia() {
+export const AdminHomePage = () => {
+
+    const deleteAlltrashMedia = async () => {
         return await apiPostData('blog/media/deleteall/',{})
     }
 
-    render() {
-
-        return (
-            <Container style={{
-                padding:"6rem 0"
-            }}>
-                <Row className="pb-4">
-                    <Col>
-                        <MyBreadCrumb />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs="auto">
-                        <Link to="/admin/createpost">
-                            <SuccessButton>
-                                <i className="fa-solid fa-plus"></i> Create post
-                            </SuccessButton>
-                        </Link>
-                    </Col>
-                    <Col xs="auto">
-                        <BasicButton onClick={() => this.deleteAlltrashMedia()}>
-                            Delete all trash
-                        </BasicButton>
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
+    return (
+        <Container style={{
+            padding:"6rem 0"
+        }}>
+            <Row className="pb-4">
+                <Col>
+                    <MyBreadCrumb />
+                </Col>
+            </Row>
+            <Row>
+                <Col xs="auto">
+                    <Link to="/admin/createpost">
+                        <SuccessButton>
+                            <i className="fa-solid fa-plus"></i> Create post
+                        </SuccessButton>
+                    </Link>
+                </Col>
+                <Col xs="auto">
+                    <BasicButton onClick={() => deleteAlltrashMedia()}>
+                        Delete all trash
+                    </BasicButton>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
