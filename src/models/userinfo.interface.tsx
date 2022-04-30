db0f6fd24340c2186e5746179a61762879f6c320
Lê Hoàng Huy Thanh <93971600+lehoanghuythanh96@users.noreply.github.com>
2022-04-30 20:54:22 +0200

    Change user model interface, change user role for menuItems vs MenuCard

--- src/models/userinfo.interface.tsx
+++ src/models/userinfo.interface.tsx
@@ -1,11 +1,8 @@
 export interface UserInfo {
     id: string
     email: string
-    user_role: userRoles
     full_name: string
-}
-
-export enum userRoles {
-    admin = "admin",
-    subscriber = "subscriber"
+    is_superuser: boolean
+    is_staff: boolean
+    is_active: boolean
 }
