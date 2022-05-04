73e52d04e1429886f50c2839c25cf1e9949d0309
Lê Hoàng Huy Thanh <93971600+lehoanghuythanh96@users.noreply.github.com>
2022-05-04 19:27:05 +0200

    change admin panel home to functional component

--- src/components/administrator/homepanel.tsx
+++ src/components/administrator/homepanel.tsx
@@ -5,47 +5,44 @@ import {Link} from "react-router-dom";
 import {apiPostData} from "../../environments/apiHandler";
 import {MyBreadCrumb} from "../UI_Components/UI_Breadcrumbs";
 
-export class AdminHomePage extends React.Component {
+export const AdminHomePage = () => {
 
-    async deleteAlltrashMedia() {
-        return await apiPostData('blog/media/deleteall/',{})
+    let deleteAlltrashMedia = async () => {
+        return await apiPostData('blog/media/deleteall/', {})
     }
 
-    async deleteAllBlogPost() {
-        return await apiPostData('blog/post/deleteall/',{})
+    let deleteAllBlogPost = async () => {
+        return await apiPostData('blog/post/deleteall/', {})
     }
 
-    render() {
-
-        return (
-            <Container style={{
-                padding:"6rem 0"
-            }}>
-                <Row className="pb-4">
-                    <Col>
-                        <MyBreadCrumb />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs="auto">
-                        <Link to="/admin/createpost">
-                            <SuccessButton>
-                                <i className="fa-solid fa-plus"></i> Create post
-                            </SuccessButton>
-                        </Link>
-                    </Col>
-                    <Col xs="auto">
-                        <BasicButton onClick={() => this.deleteAlltrashMedia()}>
-                            Delete all trash
-                        </BasicButton>
-                    </Col>
-                    <Col xs="auto">
-                        <BasicButton onClick={() => this.deleteAllBlogPost()}>
-                            Delete all blog post
-                        </BasicButton>
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
+    return (
+        <Container style={{
+            padding: "6rem 0"
+        }}>
+            <Row className="pb-4">
+                <Col>
+                    <MyBreadCrumb/>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs="auto">
+                    <Link to="/admin/createpost">
+                        <SuccessButton>
+                            <i className="fa-solid fa-plus"></i> Create post
+                        </SuccessButton>
+                    </Link>
+                </Col>
+                <Col xs="auto">
+                    <BasicButton onClick={() => deleteAlltrashMedia()}>
+                        Delete all trash
+                    </BasicButton>
+                </Col>
+                <Col xs="auto">
+                    <BasicButton onClick={() => deleteAllBlogPost()}>
+                        Delete all blog post
+                    </BasicButton>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
